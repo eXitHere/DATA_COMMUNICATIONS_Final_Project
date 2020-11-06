0847ddb478d149203f0a422e8546a6cb3a8dfd0c
swekiiz <best_n.dol@hotmail.com>
2020-11-07 02:50:17 +0700

    🎭 add .gitignore for data science(python), install virtualenv for virtual environment & test it

---
+++ project-manager/PC2/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# numpy test
+
+if __name__ == "__main__":
+    a = np.array([1, 2, 3])
+
+    print(a)
+
+    its_work = not False
+
+    if its_work:
+        print('It\'s work ! ')
