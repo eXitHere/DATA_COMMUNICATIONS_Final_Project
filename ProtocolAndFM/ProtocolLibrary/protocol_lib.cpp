fa1dac01af5f1c98d0913eba6d49e73c20b0b9cc
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2020-11-20 14:48:09 +0700

    Library: Wait for Send and Receive Code

--- ProtocolAndFM/ProtocolLibrary/protocol_lib.cpp
+++ ProtocolAndFM/ProtocolLibrary/protocol_lib.cpp
@@ -2,27 +2,35 @@
 
 
 #include<Arduino.h>
+#include"protocol_lib.h"
 
+ProtocolControl::ProtocolControl(String srcName, String startFlag)
+{
+  this->srcName = srcName;
+  this->STARTFLAG = startFlag;
+  this->allReceiving = "";
+  this->ackNo = "0";
+}
+ProtocolControl::~ProtocolControl()
+{
+}
 
-int addTwo(int a, int b)
+int ProtocolControl::addTwo(int a, int b)
 {
   return a + b;
 }
 
 
-String makeDataFrame(String textData, String frameNo)
+String ProtocolControl::makeDataFrame(String textData, String frameNo, String ENDFLAG, String destName)
 {
   /*
-  User must shift the data manually!
+    User must shift the data manually!
 
-  This code will generate a frame of 2 bytes/frame
-  Will return a frame of data in this format
-  START,dest,src,frameNo,data,data,Check,END
+    This code will generate a frame of 2 bytes/frame
+    Will return a frame of data in this format
+    START,dest,src,frameNo,data,data,Check,END
 
   */
-  const String STARTFLAG = "X";//CUSTOM THIS
-  const String ENDFLAG = "X";//CUSTOM THIS
-  const String srcName = "A", destName = "B";//CUSTOM THIS
   String toSend = "";//Frame will be store here
   //Header
   toSend += STARTFLAG;
@@ -49,23 +57,23 @@ String makeDataFrame(String textData, String frameNo)
 
   return toSend;
 }
-bool approveDataFrame(String data)//TODO: CHANGE TO CRC
+bool ProtocolControl::approveDataFrame(String frame)//TODO: CHANGE TO CRC
 {
   /*
-  Analyze checker and return boolean true if the data is correct
+    Analyze checker and return boolean true if the data is correct
   */
-    if(data.length() != 8)
+  if (frame.length() != 8)
   {
     return false;
   }
-  
+
   int sum = 0;
-  for(int i=0;i<6;i++)
+  for (int i = 0; i < 6; i++)
   {
-    sum += int(data[i]);
+    sum += int(frame[i]);
   }
 
-  if(sum%2 == (data[6]-'0'))
+  if (sum % 2 == int(frame[6] - '0'))
   {
     return true;
   }
@@ -75,18 +83,15 @@ bool approveDataFrame(String data)//TODO: CHANGE TO CRC
   }
 }
 
-String makeAckFrame(int ackNo)
+String ProtocolControl::makeAckFrame(String ackNo, String ENDFLAG, String destName)
 {
   /*
-  User must shift the data manually!
+    User must shift the data manually!
 
-  This code will generate am acknowledge frame
-  Will return a frame in this format
-  START,dest,src,frameNo,Check,END
+    This code will generate am acknowledge frame
+    Will return a frame in this format
+    START,dest,src,frameNo,Check,END
   */
-  const String STARTFLAG = "X";//CUSTOM THIS
-  const String ENDFLAG = "X";//CUSTOM THIS
-  const String srcName = "A", destName = "B";//CUSTOM THIS
   String toSend = "";//Frame will be store here
   //Header
   toSend += STARTFLAG;
@@ -108,28 +113,146 @@ String makeAckFrame(int ackNo)
   return toSend;
 }
 
-bool approveAckFrame(String data)//TODO: CHANGE TO CRC
+bool ProtocolControl::approveAckFrame(String frame)//TODO: CHANGE TO CRC
 {
   /*
-  Analyze checker and return boolean true if the ack is correct
+    Analyze checker and return boolean true if the ack is correct
   */
-  if(data.length() != 6)
+  if (frame.length() != 6)
   {
     return false;
   }
-  
+
   int sum = 0;
-  for(int i=0;i<4;i++)
+  for (int i = 0; i < 4; i++)
   {
-    sum += int(data[i]);
+    sum += int(frame[i]);
   }
 
-  if(sum%2 == (data[4]-'0'))
+  if (sum % 2 == int(frame[4] - '0'))
   {
     return true;
   }
   else
   {
     return false;
+  }
+}
+
+void ProtocolControl::transmitter()
+{
+  String frameNo = "0";
+  char receiver;
+  String textData = "";
+  int completeTrasmission = 0;
+
+  if (Serial.available())
+  {
+    ackNo = '0';//init ackNo
+    allReceiving = "";//clear receiver
+    //Serial.print("Receiver is: ");
+    while (!Serial.available());//wait for input
+    receiver = char(Serial.read());//choose receiver
+    Serial.println(receiver);
+    while (!Serial.available());//wait for input
+    textData = Serial.readStringUntil('\n');
+    Serial.println(textData);
+  }
+
+  while (textData.length() > 0)
+  {
+    String splittedData = textData.substring(0, 2);
+    String framedData = this->makeDataFrame(splittedData, frameNo, "o", "B");//String textData, String frameNo, String ENDFLAG, String destName
+    textData = textData.substring(2);//TODO: ADD IDENTIFY END OF TRANSMISSION TO FRAME: (textData.length() <= 0) ? toSend += "0" : toSend += "1";//end of data? (8 bits)
+
+    for (int i = 0; i < framedData.length(); i++)
+    {
+      //TODO: SEND DATA WITH FSK
+    }
+
+
+    //TODO: RECEIVE ACK FRAME FROM FM
+    String ackFrame = "TEMPACKFRAME";
+    while (!this->approveAckFrame(ackFrame))
+    {
+      //Wait for ACK and TIMEOUT
+      long current = millis();
+      while (!true)//TODO: WHILE NOTHING ARRIVE
+      {
+        if (millis() - current >= TIMEOUT)
+        {
+          //TIMEOUT
+          for (int i = 0; i < framedData.length(); i++)
+          {
+            //TODO: RETRASMISSION WITH FSK
+          }
+          current = millis();
+        }
+      }
+    }
+  }
+
+  frameNo = "0";
+  textData = "";
+  completeTrasmission = 0;
+  Serial.println("-----End of Trasmission-----");
+}
+
+
+String ProtocolControl::decodeFM(String receiving)
+{
+  return receiving;
+}
+
+void ProtocolControl::receiver()
+{
+  bool corrupt = false, resend=false;
+  char endFlag='1';
+  while (true)//TODO: WHILE INCOMING DATA EXIST
+  {
+    String receivedFrame = decodeFM("TEMPDATA");//TODO: RECEIVE DATA FROM ANALOG AND DECODE
+
+    char dest = receivedFrame[1];
+    if (dest == this->srcName[0])
+    {
+      char frameNumber = receivedFrame[3];
+      if (frameNumber == ackNo[0])
+      {
+        if (this->approveDataFrame(receivedFrame))
+        {
+          endFlag = receivedFrame[5];
+          ackNo == "0" ? ackNo = "1" : ackNo = "0";
+          String resAckFrame = this->makeAckFrame(ackNo, "x", "B");//TODO: CHANGE VALUE
+
+          for (int i = 0; i < resAckFrame.length(); i++)
+          {
+            //TODO: SEND ACK FRAME WITH FSK
+          }
+        }
+        else
+        {
+          Serial.println("Corrupt Frame");
+          corrupt = !corrupt;
+        }
+      }
+      else
+      {
+        Serial.println("Discard Old Frame");
+        String resAckFrame = this->makeAckFrame(ackNo, "x", "B");//TODO: CHANGE VALUE
+        resend = !resend;
+        for (int i = 0; i < resAckFrame.length(); i++)
+        {
+          //TODO: SEND ACK FRAME WITH FSK
+        }
+        
+      }
+    }
+  }
+  if(endFlag == '0' && !corrupt && !resend)
+  {
+    ackNo = "0";
+      allReceiving = "";
+      Serial.print("\n");
+      Serial.print("-------End Of Receiving----------\n\n");
   }
 }
--- ProtocolAndFM/ProtocolLibrary/protocol_lib.h
+++ ProtocolAndFM/ProtocolLibrary/protocol_lib.h
@@ -1,16 +1,32 @@
 #ifndef PROTOCOL_LIB_H
 #define PROTOCOL_LIB_H
 
-#include<Arduino.h>
+#include <Arduino.h>
+class ProtocolControl
+{
+public:
+  ProtocolControl(String, String);
+  ~ProtocolControl();
 
-int addTwo(int,int);//use for testing this import
+  int addTwo(int, int); //use for testing this import
 
+  String makeDataFrame(String textData, String frameNo, String ENDFLAG = "X", String destName = "B");
+  bool approveDataFrame(String);
 
-String makeDataFrame(String, int);
-bool approveDataFrame(String);
+  String makeAckFrame(String ackNo, String ENDFLAG = "X", String destName = "B");
+  bool approveAckFrame(String);
 
-String makeAckFrame(String, int);
-bool approveAckFrame(String);
+  void transmitter();
+  void receiver();
 
+  String decodeFM(String);
+
+private:
+  String srcName;
+  String ackNo;
+  String allReceiving;
+  String STARTFLAG;
+  const int TIMEOUT = 2000;
+};
 
 #endif PROTOCOL_LIB_H
